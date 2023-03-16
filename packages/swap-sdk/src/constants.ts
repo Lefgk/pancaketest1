e26af4feb0dd78929e0988287cbc457f412742d1
LG <lefgiak@gmail.com>
2023-03-16 16:55:40 +0000

    v1

---
+++ packages/swap-sdk/src/constants.ts
@@ -0,0 +1,107 @@
+import { Percent } from '@pancakeswap/swap-sdk-core'
+import { ERC20Token } from './entities/token'
+
+export enum ChainId {
+  ETHEREUM = 1,
+  GOERLI = 5,
+  BSC = 56,
+  BSC_TESTNET = 97,
+}
+
+export const ZERO_PERCENT = new Percent('0')
+export const ONE_HUNDRED_PERCENT = new Percent('1')
+
+export const FACTORY_ADDRESS = '0x98528e5B5A2a125A718001932DdeA27db192A60d'
+
+const FACTORY_ADDRESS_ETH = '0x1097053Fd2ea711dad45caCcc45EfF7548fCB362'
+
+export const FACTORY_ADDRESS_MAP: Record<number, string> = {
+  [ChainId.ETHEREUM]: FACTORY_ADDRESS_ETH,
+  [ChainId.GOERLI]: FACTORY_ADDRESS_ETH,
+  [ChainId.BSC]: FACTORY_ADDRESS,
+  [ChainId.BSC_TESTNET]: '0x98528e5B5A2a125A718001932DdeA27db192A60d',
+}
+export const INIT_CODE_HASH = '0xc0d1bb14e30d7fa5348fde679df2d80c8e024df2571026027c26af0f3a779fcd'
+
+const INIT_CODE_HASH_ETH = '0xc0d1bb14e30d7fa5348fde679df2d80c8e024df2571026027c26af0f3a779fcd'
+export const INIT_CODE_HASH_MAP: Record<number, string> = {
+  [ChainId.ETHEREUM]: INIT_CODE_HASH_ETH,
+  [ChainId.GOERLI]: INIT_CODE_HASH_ETH,
+  [ChainId.BSC]: INIT_CODE_HASH,
+  [ChainId.BSC_TESTNET]: '0xc0d1bb14e30d7fa5348fde679df2d80c8e024df2571026027c26af0f3a779fcd',
+}
+
+export const WETH9 = {
+  [ChainId.ETHEREUM]: new ERC20Token(
+    ChainId.ETHEREUM,
+    '0xC02aaA39b223FE8D0A0e5C4F27eAD9083C756Cc2',
+    18,
+    'WETH',
+    'Wrapped Ether',
+    'https://weth.io'
+  ),
+  [ChainId.GOERLI]: new ERC20Token(
+    ChainId.GOERLI,
+    '0xB4FBF271143F4FBf7B91A5ded31805e42b2208d6',
+    18,
+    'WETH',
+    'Wrapped Ether',
+    'https://weth.io'
+  ),
+}
+
+export const WBNB = {
+  [ChainId.ETHEREUM]: new ERC20Token(
+    ChainId.ETHEREUM,
+    '0x418D75f65a02b3D53B2418FB8E1fe493759c7605',
+    18,
+    'WBNB',
+    'Wrapped BNB',
+    'https://www.binance.org'
+  ),
+  [ChainId.BSC]: new ERC20Token(
+    ChainId.BSC,
+    '0xbb4CdB9CBd36B01bD1cBaEBF2De08d9173bc095c',
+    18,
+    'WBNB',
+    'Wrapped BNB',
+    'https://www.binance.org'
+  ),
+  [ChainId.BSC_TESTNET]: new ERC20Token(
+    ChainId.BSC_TESTNET,
+    '0xae13d989daC2f0dEbFf460aC112a837C89BAa7cd',
+    18,
+    'WBNB',
+    'Wrapped BNB',
+    'https://www.binance.org'
+  ),
+}
+
+export const WNATIVE: Record<number, ERC20Token> = {
+  [ChainId.ETHEREUM]: WETH9[ChainId.ETHEREUM],
+  [ChainId.GOERLI]: WETH9[ChainId.GOERLI],
+  [ChainId.BSC]: WBNB[ChainId.BSC],
+  [ChainId.BSC_TESTNET]: WBNB[ChainId.BSC_TESTNET],
+}
+
+export const NATIVE: Record<
+  number,
+  {
+    name: string
+    symbol: string
+    decimals: number
+  }
+> = {
+  [ChainId.ETHEREUM]: { name: 'Ether', symbol: 'ETH', decimals: 18 },
+  [ChainId.GOERLI]: { name: 'Goerli Ether', symbol: 'GOR', decimals: 18 },
+  [ChainId.BSC]: {
+    name: 'Binance Chain Native Token',
+    symbol: 'BNB',
+    decimals: 18,
+  },
+  [ChainId.BSC_TESTNET]: {
+    name: 'Binance Chain Native Token',
+    symbol: 'tBNB',
+    decimals: 18,
+  },
+}
